aea44b72498521244f6c25631f7117a200b97712
zm20304 <zm20304@josai.ac.jp>
2022-02-07 07:53:07 +0000

    311

---
+++ ex6-3.js
@@ -0,0 +1,16 @@
+function hyoji() { 
+    //表示する文字
+    var str= "教授のおかげで知識が付きました。"; 
+    //テキストボックスの文字数
+    var cnt= document.timer.moji.value.length;
+     //文字が全部表示されているか確認
+     if ( cnt< 16 ) { 
+       //現在より1文字多く切り出して表示
+       document.timer.moji.value= str.substr(0,cnt+1); } 
+       else { 
+         //全て表示されたら、空文字に戻す
+         document.timer.moji.value= ""; } } 
+         function startfnc() { 
+           //関数hyoji()を1000ミリ秒間隔で呼び出す
+           setInterval("hyoji()",1000); } 
+           function mes() { alert("授業ありがとうございました！"); }
