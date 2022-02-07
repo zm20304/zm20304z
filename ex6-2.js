19ee75620d291049350c53ba700d8c583dfa0e33
zm20304 <zm20304@josai.ac.jp>
2022-02-07 07:03:44 +0000

    311

--- ex6-2.js
+++ ex6-2.js
@@ -1,2 +1,2 @@
-function imgchange1() {document.getElementById('logo').src= './image/keiei_small.jpg';}
+function imgchange1() {document.getElementById('logo').src= './image/index_small.jpg';}
 function imgchange2() {document.getElementById('logo').src= './image/ju旗_small.jpg.png';}
